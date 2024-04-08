84d75d282c42cc7fa98596988d404ba1bb543ec9
artem <ap2774431@gmail.com>
2024-04-08 13:03:27 +0300

    Change structure

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'google_map.dart';
 
 
 class MapScreen extends StatefulWidget {
@@ -21,11 +22,11 @@ class _MyAppState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.center,
       children: [
         Map(),
         Positioned(
           bottom: 10,
-          right: 10,
           child: ElevatedButton(
             style: ButtonStyle(
                 backgroundColor: MaterialStatePropertyAll(Colors.blue),
@@ -51,35 +52,3 @@ class _MyAppState extends State<MapScreen> {
   }
 }
 
-class Map extends StatefulWidget {
-  @override
-  _MapState createState() => _MapState();
-}
-
-class _MapState extends State<Map> {
-  GoogleMapController? mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: GoogleMap(
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        mapType: MapType.normal,
-        zoomGesturesEnabled: true,
-        zoomControlsEnabled: true,
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
-      )
-    );
-  }
-}
